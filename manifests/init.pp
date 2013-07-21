f8f5153ffb99620eb244686c4a8faecd8af4ce78
Nic Waller <nicwaller@gmail.com>
2013-07-21 09:14:09 -0700

    Changed sssd.conf mode to 400 and cleaned up tab formatting
    Since local users shouldn't be editing sssd.conf anyways, remove the
    owner write bit.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@
 # [*filter_groups*]
 # Optional. Array. Default is 'root'. Exclude specific groups from being
 # fetched using sssd. This is particularly useful for system accounts.
-# 
+#
 # === Requires
 # - [ripienaar/concat]
 # - [puppetlab/stdlib]
@@ -28,26 +28,26 @@ class sssd (
   $filter_users    = [ 'root' ],
   $filter_groups   = [ 'root' ]
 ) {
-	validate_array($domains)
-	validate_array($filter_users)
-	validate_array($filter_groups)
+  validate_array($domains)
+  validate_array($filter_users)
+  validate_array($filter_groups)
 
-	concat::fragment{ 'sssd_conf_header':
-		target  => 'sssd_conf',
-		content => template('sssd/header_sssd.conf.erb'),
-		order   => 10,
-	}
+  concat::fragment{ 'sssd_conf_header':
+    target  => 'sssd_conf',
+    content => template('sssd/header_sssd.conf.erb'),
+    order   => 10,
+  }
 
-	package { 'sssd':
-		ensure  => installed,
-	} -> concat { 'sssd_conf':
-		path	=> '/etc/sssd/sssd.conf',
-		mode	=> 600,
-	} ~> exec { 'authconfig-sssd':
-		command	=> '/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update',
-		refreshonly	=> true,
-	} ~> service { 'sssd':
-		ensure	=> running,
-		enable	=> true,
-	}
+  package { 'sssd':
+    ensure      => installed,
+  } -> concat { 'sssd_conf':
+    path        => '/etc/sssd/sssd.conf',
+    mode        => 400,
+  } ~> exec { 'authconfig-sssd':
+    command     => '/usr/sbin/authconfig --enablesssd --enablesssdauth --enablelocauthorize --update',
+    refreshonly => true,
+  } ~> service { 'sssd':
+    ensure      => running,
+    enable      => true,
+  }
 }
