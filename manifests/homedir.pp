48e0541c0e3bde5921fef82ac310702ef596149b
Nic Waller <code@nicwaller.com>
2013-07-23 03:03:09 +0000

    Only install oddjobd on RHEL 6. And if selinux enabled on RHEL 5, then bail out.

--- manifests/homedir.pp
+++ manifests/homedir.pp
@@ -16,31 +16,40 @@
 # Copyright 2013 Nicholas Waller, unless otherwise noted.
 #
 class sssd::homedir {
-  # In RHEL6, messagebus is not started by default.  
-  service { 'messagebus':
-    ensure    => running,
-    enable    => true,
-    # If hasstatus is not set to true, messagebus will restart EVERY time.
-    # Does anyone know why?
-    hasstatus => true,
-  }
-
-  package { 'oddjob-mkhomedir':
-    ensure => installed,
-    notify => Exec['authconfig-mkhomedir'],
-  }
-
-  service { 'oddjobd':
-    ensure  => running,
-    enable  => true,
-    require => [ Package['oddjob-mkhomedir'], Service['messagebus'] ],
+  if $osfamily == 'RedHat' and versioncmp($operatingsystemrelease,'6.0') >= 0 {
+    $reqs = [ Service['messagebus'], Service['oddjobd'] ]
+    # In RHEL6, messagebus is not started by default.  
+    service { 'messagebus':
+      ensure    => running,
+      enable    => true,
+      # If hasstatus is not set to true, messagebus will restart EVERY time.
+      # Does anyone know why?
+      hasstatus => true,
+    }
+  
+    package { 'oddjob-mkhomedir':
+      ensure => installed,
+      notify => Exec['authconfig-mkhomedir'],
+    }
+  
+    service { 'oddjobd':
+      ensure  => running,
+      enable  => true,
+      require => [ Package['oddjob-mkhomedir'], Service['messagebus'] ],
+    }
+  } elsif $osfamily == 'RedHat' and versioncmp($operatingsystemrelease,'6.0') < 0 {
+    # pam_mkhomedir.so is already installed as part of pam.
+    # facter is true if selinux is enabled, EVEN in permissive mode
+    if $selinux == 'true' {
+      fail('pam_mkhomedir.so required when using RHEL < 6, but pam_mkhomedir.so is not compatible with selinux.')
+    }
   }
 
   # We always need to start the sssd service after calling --mkhomedir.
   exec { 'authconfig-mkhomedir':
     command     => '/usr/sbin/authconfig --enablemkhomedir --update',
     refreshonly => true,
-    require     => [ Service['messagebus'], Service['oddjobd'] ],
+    require     => $reqs,
     notify      => Exec[ 'authconfig-sssd' ], 
   }
 }
